015fedf4bb1fcb9da9a2157dd0ecbc88b9f9ab5b
Muhammad Naimul Islam <naimulislam19149@gmail.com>
2022-08-14 22:32:11 +0600

    prescription page made interactive with alpine.js

--- prescribe/script.js
+++ prescribe/script.js
@@ -1,6 +1,35 @@
 import "@styles/prescribe/prescribe.scss";
-// import Alpine from "alpinejs";
+import Alpine from "alpinejs";
 
-// window.Alpine = Alpine;
+Alpine.store("prescription", {
+  currMed: {
+    name: "",
+    intakeAfterMeal: 0,
+    atBreakfast: 0,
+    atlunch: 0,
+    atDinner: 0,
+    remarks: "",
+  },
+  medicines: [],
+  resetCurrMed() {
+    this.currMed = {
+      name: "",
+      intakeAfterMeal: true,
+      atBreakfast: false,
+      atlunch: false,
+      atDinner: false,
+      remarks: "",
+    };
+  },
+  addMedicine() {
+    this.medicines.push({ ...this.currMed, id: this.medicines.length + 1 });
+    this.resetCurrMed();
+  },
+  deleteMedicine(id) {
+    this.medicines = this.medicines.filter((med) => med.id !== id);
+  },
+});
 
-// Alpine.start();
+window.Alpine = Alpine;
+
+Alpine.start();
